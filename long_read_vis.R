6c3151c6cc703ba2730c1266c619b9979b580a73
claudiacarter <cloudycarter@gmail.com>
2024-08-14 21:56:25 +0100

    code formatting

--- long_read_vis.R
+++ long_read_vis.R
@@ -6,6 +6,7 @@ c3_m6A_site <- read.csv('c3_data.site_proba_tra_filtered.csv')
 i2_m6A_site <- read.csv('i2_data.site_proba_tra_filtered.csv')
 i3_m6A_site <- read.csv('i3_data.site_proba_tra_filtered.csv')
 
+#==visualising the DRACH motifs methylated==
 motifs_c2 <- as.vector(c2_m6A_site$kmer)
 motifs_c3 <- as.vector(c3_m6A_site$kmer)
 motifs_i2 <- as.vector(i2_m6A_site$kmer)
@@ -14,7 +15,7 @@ motifs_i3 <- as.vector(i3_m6A_site$kmer)
 motifs_c <- as.vector(c(motifs_c2, motifs_c3))
 motifs_i <- as.vector(c(motifs_i2, motifs_i3))
 
-#==convert Us to Ts for true motifs==
+# convert U to T
 motifs_c2 <- gsub(pattern = "T", replacement = "U", x = motifs_c2)
 motifs_c3 <- gsub(pattern = "T", replacement = "U", x = motifs_c3)
 motifs_i2 <- gsub(pattern = "T", replacement = "U", x = motifs_i2)
@@ -24,12 +25,12 @@ motifs_c <- gsub(pattern = "T", replacement = "U", x = motifs_c)
 motifs_i <- gsub(pattern = "T", replacement = "U", x = motifs_i)
 
 
-#==create custom colour scheme for colourblind accessibility==
+# create custom colour scheme for colourblind accessibility
 colours = make_col_scheme(chars=c('A', 'U', 'C', 'G'),
                       cols=c('#40B0A6', '#B32240', '#244061', '#FFC107'))
 #B32213 - other 'red' option not used
 
-#==generate sequence logos and align plots==
+# generate sequence logos and align plots
 # plots per sample
 c2_logo <- ggseqlogo(motifs_c2,
                      method = 'prob',
@@ -76,3 +77,6 @@ infected_logo <- ggseqlogo(motifs_i,
                                                  face = "bold"))
 
 plot_grid(control_logo, infected_logo, ncol = 1, align = 'v')
+
+#==compare transcript methylation levels==
+
