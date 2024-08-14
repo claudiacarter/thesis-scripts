3b6ef8f2ff3deb7d8aa76c8bebe165710d9d68dd
claudiacarter <cloudycarter@gmail.com>
2024-08-14 21:56:49 +0100

    code formatting

--- poly_a.R
+++ poly_a.R
@@ -1,7 +1,7 @@
 library(readr)
 library(dplyr)
 
-# read in nanopolish output CSVs
+# read in nanopolish polya output CSVs
 np_out_c2 <- as.data.frame(read_tsv("c2_polya_out.tsv"))
 np_pass_c2 <- filter(np_out_c2, qc_tag == "PASS")
 
@@ -13,3 +13,5 @@ np_pass_i2 <- filter(np_out_i2, qc_tag == "PASS")
 
 np_out_i3 <- as.data.frame(read_tsv("i3_polya_out.tsv"))
 np_pass_i3 <- filter(np_out_i3, qc_tag == "PASS")
+
+#
