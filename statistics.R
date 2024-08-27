caefae8535484c7e44b0a292d9af4022a324e19d
claudiacarter <cloudycarter@gmail.com>
2024-08-27 20:08:49 +0100

    add hypergeometric

--- statistics.R
+++ statistics.R
@@ -9,3 +9,10 @@ sink(file=NULL)
 sink(file="t_test.txt")
 t.test(control_percentmeth, infected_percent_meth, var.equal = TRUE)
 sink(file=NULL)
+
+# check significance of GDMT enrichment in DEGs with hypergeometric test
+deg_group = 672
+gdmt_group = 665
+group_overlap = 16
+rn_genes = 23194
+phyper(group_overlap,deg_group, rn_genes-deg_group, gdmt_group, lower.tail=FALSE)
